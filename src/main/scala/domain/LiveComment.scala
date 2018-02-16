2fbc977d55b277dca69a45aa253061c0c2752e5f
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2018-02-16 15:59:13 +0200

    Add more details to model

--- src/main/scala/domain/LiveComment.scala
+++ src/main/scala/domain/LiveComment.scala
@@ -15,13 +15,46 @@ final case class LiveComment(
   messageTags : MessageTags,
   video       : Video)
 
+final case class ProfileId(value: String)
+
+sealed trait Profile
+
+final case class EntityAtTextRange(
+  id: ProfileId,
+  length: Int,
+  name: String,
+  profile: Profile,
+  offset: Int,
+  entityType: EntityType
+)
+
+sealed trait EntityType
+
+case object UserType extends EntityType
+case object PageType extends EntityType
+case object EventType extends EntityType
+case object GroupType extends EntityType
+case object ApplicationType extends EntityType
+
+final case class StoryAttachmentMedia(image: ImageSource)
+final case class ImageSource(height: Double, source: String, width: Double)
+
+final case class StoryAttachment(
+  description      : String,
+  descriptionTags  : List[EntityAtTextRange],
+  title            : String,
+  typeOfAttachment : String,
+  url              : String,
+  target           : StoryAttachmentTarget,
+  media            : StoryAttachmentMedia)
+
 final case class StoryAttachmentId(value: String)
 
-final case class StoryAttachment(id: StoryAttachmentId)
+final case class StoryAttachmentTarget(id: StoryAttachmentId, url: String)
 
 final case class FacebookUserId(value: String)
 
-final case class FacebookUser(id: FacebookUserId)
+final case class FacebookUser(id: FacebookUserId) extends Profile
 
 final case class MessageTagId(value: String)
 
