851bbb07fc01f26e743b10acff989341b434d58f
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2018-05-03 16:02:53 +0300

    Streaming client prototype

--- src/main/scala/app/StreamingApp.scala
+++ src/main/scala/app/StreamingApp.scala
@@ -5,14 +5,14 @@ import akka.stream.ActorMaterializer
 import client.FacebookStreamingClient
 import domain.oauth.{FacebookAccessToken, TokenValue, UserAccessToken}
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
 
 object StreamingApp extends App {
 
   implicit val actorSystem = ActorSystem()
   implicit val materializer = ActorMaterializer()
+  implicit val ec = actorSystem.dispatcher
   val token = "EAAcAL79ZCFjMBAHiFoLrfmPVpuZAwtNHd2PdGaQ4BSBiZCZBJACy3jiKEimNhava1JyOQbWOSUEbefM533wnyw9vaTDkgSG7tHROqjIFSC2o4fw1d7yqxYam9ZBu0yoZCan7kXRIhcqjZA8X0nLV7cXuaZA95234corjhN2LZAxlmCBk5L3pTEcBFZAHRrn7aSpZAbfJoyzEDFjRAZDZD"
   val client = FacebookStreamingClient(FacebookAccessToken(TokenValue(token), UserAccessToken.notSpecified))
-  Await.result(client.liveComments(), Duration.Inf)
+  client.liveComments()
 }
--- src/main/scala/client/FacebookStreamingClient.scala
+++ src/main/scala/client/FacebookStreamingClient.scala
@@ -3,8 +3,9 @@ package client
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ThrottleMode}
 import akka.stream.alpakka.sse.scaladsl.EventSource
+import akka.stream.scaladsl.Sink
 import config.FacebookConfig
 import domain.oauth.FacebookAccessToken
 
@@ -12,14 +13,23 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 
 class FacebookStreamingClient(accessToken: FacebookAccessToken)(implicit actorSystem: ActorSystem, materializer: ActorMaterializer) {
-  val uri = "streaming-graph.facebook.com/193632854762744/live_comments"
+  val uri = "http://streaming-graph.facebook.com/202059110586785/live_comments?access_token=EAAcAL79ZCFjMBAOhVcq78FWl5i3RmZBSBTHEgjajCMmNW8Ypr7OB6HTEbBp4XqBkAQ20ZAeSkI6VtjN2fbXEWlJ5UyZAhfSVfpP6SOFTtMZBO04OexkNx9dJmDbAjcdQiH28zLKOZBYlsJG2NnJjoRp5oyF8aZCDYaUXrbhQWOz93nLYclvMZAZA2Skf5UDG482Gfe58SA5xz1gZDZD&comment_rate=one_per_two_seconds&fields=from{id,name},message"
 
-  def send(request: HttpRequest): Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = uri))
+  implicit val ec = actorSystem.dispatcher
+
+  def send(request: HttpRequest): Future[HttpResponse] = {
+    val result = Http().singleRequest(HttpRequest(uri = uri))
+    result onComplete println
+    result
+  }
   val eventSource = EventSource(Uri(uri), send, Some("2"), 1.second)
 
-
   def liveComments() = {
-    eventSource.runForeach(e => println(e.data))(materializer)
+
+    eventSource.runForeach(serverEvent => println(serverEvent))
+
+    val events = eventSource.throttle(1, 500.milliseconds, 5, ThrottleMode.Shaping).take(5).runWith(Sink.seq)
+    events.onComplete(ev => println(ev))
   }
 }
 
